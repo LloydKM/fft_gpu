0682598025829966816edf4ebc1f672a6283e160
LloydKM <benedict.hans@ovgu.de>
2017-12-21 15:04:00 +0100

    cmake does what it should... hope it stays that wayy

--- cpu/src/main.cpp
+++ cpu/src/main.cpp
@@ -17,7 +17,7 @@ int main() {
     //end timing
     clock_t tend = clock();
     //final tim
-    double telapsed = (double)/(tend - tstart) / CLOCKS_PER_SEC;
+    double telapsed = (double)(tend - tstart) / CLOCKS_PER_SEC;
     printf( "time elapsed = %2.4f sec\n", telapsed);
 
     //TODO cleanup memory
