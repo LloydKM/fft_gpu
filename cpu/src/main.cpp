8e300b4f385a2bf83338a8210170dd6d55d0af60
LloydKM <benedict.hans@ovgu.de>
2017-12-22 17:03:43 +0100

    now opens soundfiles - still problems on memory cleanup

--- cpu/src/main.cpp
+++ cpu/src/main.cpp
@@ -1,26 +1,57 @@
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <fftw3.h>
-#include <sndfile.hh>
+#include <sndfile.h>
 
-int main() {
-    //TODO crazy shit with sndfile
+//Linux implementation
 
-    //TODO allocating
+int main(int argc, char** argv) {
 
-    //TODO initializing
+    //set SF_INFO to properly open file to read
+    SF_INFO read_info;
+    read_info.format = 0;
+
+    char* fname;
+
+    //use test file if none is passed
+    if( argc < 2) {
+        fname = (char*) malloc(16 * sizeof(char));
+        fname = "../src/test.wav";
+    } else {
+        fname = (char*) malloc(strlen(argv[1]) * sizeof(char));
+        fname = argv[1];
+    }
+
+    SNDFILE* input = sf_open(fname, SFM_READ, &read_info);
+    if (input == NULL){
+        printf("YOU FAIL!\n");
+        return -1;
+    }
 
     //start timing
     clock_t tstart = clock();
 
     //TODO calculating
+    //fftw
+    #if 1
+
+    //own fft
+    #else
+
+    #endif
 
     //end timing
     clock_t tend = clock();
     //final tim
-    double telapsed = (double)(tend - tstart) / CLOCKS_PER_SEC;
+    double telapsed = (double)(tend - tstart)    / CLOCKS_PER_SEC;
     printf( "time elapsed = %2.4f sec\n", telapsed);
 
     //TODO cleanup memory
+    free(fname);
+    if (sf_close(input) != 0) printf("cleanup failed!\n");
 
 	return 0;
 }
